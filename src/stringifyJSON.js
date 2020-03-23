2ff20f3d9d38b64ead1b80f80b4f96e0866e925a
Jacob Orel <jakeorel@gmail.com>
2020-03-23 14:50:09 -0500

    "added input for arrays"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -6,29 +6,45 @@
 var stringifyJSON = function(obj) {
   //if object is null
   if(obj === null){
-      return "null";
+    return "null";
   }
   //if object is undefined
   if(obj === undefined){
-      return undefined
+    return undefined;
   }
-  //if object is true
-  //if object is false
+  //if obj is boolean
   if(typeof obj === "boolean"){
-      return '' + obj;
+    return '' + obj;
   }
-
   //if object is string
   if(typeof obj === "string"){
-      return '"' + obj + '"';
+    return '"' + obj + '"';
   }
   //if object is number
- if(typeof obj === "number"){
+  if(typeof obj === "number"){
     return '' + obj;
- }
+  }
   //if object is function
   if(typeof obj === "function"){
-      return undefined;
+    return undefined;
   }
+  //check if the obj is an array
+  //if so we need to push it into an array
+  var resultArr = [];
+  if(Array.isArray(obj)){
+    //if the array is empty
+    if(!obj.length){
+      return '[]';
+    }
+    //check the values
+    for (var i = 0; i < obj.length; i++){
+      //if those are also array
+      resultArr.push(stringifyJSON(obj[i]));
+      //we will recurse it
+    }
+
+    return  '[' + resultArr + ']';
+  }
+
 };
 
