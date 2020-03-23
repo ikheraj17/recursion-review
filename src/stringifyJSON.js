5cb81f8c10c0762d25349efaf725ec9a7cd97ae6
Jacob Orel <jakeorel@gmail.com>
2020-03-23 13:15:43 -0500

    "added tests for primitive data values and function value"

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,31 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //if object is null
+  if(obj === null){
+      return "null";
+  }
+  //if object is undefined
+  if(obj === undefined){
+      return undefined
+  }
+  //if object is true
+  //if object is false
+  if(typeof obj === "boolean"){
+      return '' + obj;
+  }
+
+  //if object is string
+  if(typeof obj === "string"){
+      return '"' + obj + '"';
+  }
+  //if object is number
+ if(typeof obj === "number"){
+    return '' + obj;
+ }
+  //if object is function
+  if(typeof obj === "function"){
+      return undefined;
+  }
 };
+
